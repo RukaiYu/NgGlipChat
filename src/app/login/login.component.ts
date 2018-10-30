622409bf1f8ae9371badcf1aa6657b30856e3693
Rukai Yu <rukai.yu@outlook.com>
2018-10-30 21:26:56 +0800

    Stuff (#1)
    * login page
    
    * login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { ApiService } from '../../shared/services/api/api.service';
+import config from '../../config';
+import framework7 from '../../framework7';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _api: ApiService) {
+    this.oauthUri = _api.Rc.authorizeUri(config.OAUTH_REDIRECT_URI);
+  }
+
+  @Output()
+  oauthUri: string;
 
   ngOnInit() {
+  }
+
+  onLoginButtonClicked(event) {
+    framework7.popup.open('#login-popup', true);
   }
 
 }
---
+++ src/framework7.ts
@@ -0,0 +1,9 @@
+import Cookies from 'js-cookie';
+import Framework7 from 'framework7/js/framework7';
+
+import 'framework7/css/framework7.css';
+
+const theme = Cookies.get('FRAMEWORK7_THEME') || 'ios';
+const framework7 = new Framework7({ theme });
+
+export default framework7;
